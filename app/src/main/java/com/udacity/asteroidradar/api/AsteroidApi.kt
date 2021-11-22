04f1c3af213dd587389635776ef4f6f478827256
Vivian Pereira <vivispereira.21@gmail.com>
2021-11-23 09:57:21 +1000

    Creating asteroids API

---
+++ app/src/main/java/com/udacity/asteroidradar/api/AsteroidApi.kt
@@ -0,0 +1,24 @@
+package com.udacity.asteroidradar.api
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.converter.scalars.ScalarsConverterFactory
+
+object AsteroidApi {
+    private val moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(ScalarsConverterFactory.create())
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .baseUrl(Constants.BASE_URL)
+        .build()
+
+    val service: AsteroidApiService by lazy { retrofit.create(AsteroidApiService::class.java) }
+}
---
+++ app/src/main/java/com/udacity/asteroidradar/api/AsteroidApiService.kt
@@ -0,0 +1,20 @@
+package com.udacity.asteroidradar.api
+
+import com.udacity.asteroidradar.Constants
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+
+interface AsteroidApiService {
+
+    @GET("neo/rest/v1/feed")
+    suspend fun getAsteroids(
+        @Query("start_date") startDate: String,
+        @Query("end_date") endDate: String,
+        @Query("api_key") key: String = Constants.API_KEY)
+
+    @GET("planetary/apod")
+    suspend fun getDailyPicture(
+        @Query("api_key") key: String = Constants.API_KEY)
+}
+
